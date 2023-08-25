15caa6520eeffdd9e2a70cd2eeb6411ddc91018b
phanchithien <phanchithien456@gmail.com>
2023-08-26 00:11:03 +0700

    set title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Thien Phan';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Thien Phan';
 
 export default function Layout({ children, home }) {
     return (
